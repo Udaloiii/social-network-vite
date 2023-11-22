5b12456829ea87c34e3a2108b6a9043e67ef44f6
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-22 21:56:59 +0300

    added request for receiving messages

--- src/layout/messages/Messages.tsx
+++ src/layout/messages/Messages.tsx
@@ -1,15 +1,57 @@
 import styled from "styled-components";
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
+import {FlexWrapper} from "../../components/flexWrapper/FlexWrapper";
+import {Loader} from "../../components/loader/Loader";
 
-export const Messages:FC = () => {
+
+type MessagesType = {
+    postId: number
+    id: number
+    name: string
+    email: string
+    body: string
+}
+export const Messages: FC = () => {
+    const [messages, setMessages] = useState<null | MessagesType[]>(null)
+    useEffect(() => {
+        setTimeout(() => {
+                fetch('https://jsonplaceholder.typicode.com/comments')
+                    .then(response => response.json())
+                    .then(json => {
+                        setMessages(json)
+                    })
+            console.log("timeout")
+            }, 2000
+        )
+    }, []);
     return (
-        <StyleMessages>
+            messages ? <StyleMessages>
+                    <FlexWrapper direction={"column"} gap={"20px"}>
+                        {messages?.map(el => {
+                            return <StyleBlock key={el.id}>
+                                <span>{el.id}.</span>
+                                <span>{el.name}</span>
+                                <span>{el.email}</span>
+                                <span>{el.body}</span>
+                            </StyleBlock>
+                        })}
+                    </FlexWrapper>
+                </StyleMessages>
+                : <Loader/>
 
-        </StyleMessages>
+
     )
 }
 
 const StyleMessages = styled.section`
   background-color: #e54a6b;
-  flex-grow: 1;
+  //flex-grow: 1;
+  width: calc (100vw - 150px);
+`
+const StyleBlock = styled.div`
+  flex: 0 0 auto;
+
+  span {
+    margin-right: 20px;
+  }
 `
