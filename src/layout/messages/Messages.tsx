27377d6bf5e35247b143df6b55caf0e34706e421
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-23 13:49:10 +0300

    changed Messages.tsx & News.tsx - added axios-request; changed style

--- src/layout/messages/Messages.tsx
+++ src/layout/messages/Messages.tsx
@@ -2,49 +2,39 @@ import styled from "styled-components";
 import {FC, useEffect, useState} from "react";
 import {FlexWrapper} from "../../components/flexWrapper/FlexWrapper";
 import {Loader} from "../../components/loader/Loader";
+import {messagesApi, MessagesType} from "../../api/messages-api";
 
 
-type MessagesType = {
-    postId: number
-    id: number
-    name: string
-    email: string
-    body: string
-}
+
 export const Messages: FC = () => {
     const [messages, setMessages] = useState<null | MessagesType[]>(null)
     useEffect(() => {
-        setTimeout(() => {
-                fetch('https://jsonplaceholder.typicode.com/comments')
-                    .then(response => response.json())
-                    .then(json => {
-                        setMessages(json)
-                    })
-            console.log("timeout")
-            }, 2000
-        )
+        messagesApi.getMessages()
+            .then(res => {
+                setMessages(res.data)
+            })
     }, []);
     return (
-            messages ? <StyleMessages>
-                    <FlexWrapper direction={"column"} gap={"20px"}>
-                        {messages?.map(el => {
-                            return <StyleBlock key={el.id}>
-                                <span>{el.id}.</span>
-                                <span>{el.name}</span>
-                                <span>{el.email}</span>
-                                <span>{el.body}</span>
-                            </StyleBlock>
-                        })}
-                    </FlexWrapper>
-                </StyleMessages>
-                : <Loader/>
+        messages ? <StyleMessages>
+                <FlexWrapper direction={"column"} gap={"20px"}>
+                    {messages?.map(el => {
+                        return <StyleBlock key={el.id}>
+                            <span>{el.id}.</span>
+                            <span>{el.email}</span>
+                            <span>{el.name}</span>
+                            <span>{el.body}</span>
+                        </StyleBlock>
+                    })}
+                </FlexWrapper>
+            </StyleMessages>
+            : <Loader/>
 
 
     )
 }
 
 const StyleMessages = styled.section`
-  background-color: #e54a6b;
+  background-color: #caead3;
   //flex-grow: 1;
   width: calc (100vw - 150px);
 `
--- src/layout/news/News.tsx
+++ src/layout/news/News.tsx
@@ -1,42 +1,37 @@
 import styled from "styled-components";
 import {FC, useEffect, useState} from "react";
-import { FlexWrapper } from "../../components/flexWrapper/FlexWrapper";
+import {FlexWrapper} from "../../components/flexWrapper/FlexWrapper";
+import {ArticlesType, newsApi} from "../../api/news-api";
 
 
-type ArticlesType = {
-    author: string
-    content: string
-    description: string
-    publishedAt: string
-    source: string
-    title: string
-    url: string
-    urlToImage: string
-}
 
-type DataType = {
-    articles: ArticlesType[]
-    status: string
-    totalResults: number
-}
 export const News: FC = () => {
     const [news, setNews] = useState<null | ArticlesType[]>(null)
     useEffect(() => {
-        fetch("https://newsapi.org/v2/everything?q=tesla&from=2023-10-22&sortBy=publishedAt&apiKey=fa43ff6276f44c29aed1a8510204d829")
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                setNews(data.articles)
+        newsApi.getNews()
+            .then(res => {
+                const filteredNews = res.data.articles.filter((article: ArticlesType) => {
+                    // Регулярное выражение для проверки наличия русских символов
+                    const russianRegex = /[а-яА-ЯЁё]/;
+                    return russianRegex.test(article.description);
+                });
+                console.log(res.data.articles)
+                setNews(filteredNews);
             })
-        // .then(res => setNews(res))
-
     }, [])
     return (
         <StyleNews>
             <FlexWrapper direction={"column"} gap={"30px"}>
                 {news?.map((el, index) => {
                     return <div key={index}>
-                        {el.description}
+                        <FlexWrapper  gap={"20px"}>
+                            <StyleImage src={el.urlToImage} alt=""/>
+                            <FlexWrapper direction={"column"} gap={"20px"}>
+                                {el.description}
+                                <StyleLink href={el.url} target={"_blank"}>подробнее</StyleLink>
+                            </FlexWrapper>
+                    </FlexWrapper>
+
                     </div>
                 })}
             </FlexWrapper>
@@ -45,6 +40,37 @@ export const News: FC = () => {
 }
 
 const StyleNews = styled.section`
-  background-color: #efb4b4;
+  background-color: #dbfff3;
   flex-grow: 1;
+
+  &:last-child {
+    padding-bottom: 15px;
+  }
+
+  ${FlexWrapper} {
+    margin: 0 10px;
+
+    &:nth-child(1) {
+      margin-top: 15px;
+    }
+  }
+`
+
+const StyleImage = styled.img`
+  width: 225px;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 4px;
+  
+
+`
+const StyleLink = styled.a`
+  color: #2e3288;
+  transition: .2s;
+  width: max-content;
+
+  &:hover {
+    color: #5559e5;
+    transition: .2s;
+  }
 `
