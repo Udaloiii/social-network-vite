367e82c61ef6cc0617542b6d034ac8b76ccd7ac2
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-25 18:25:50 +0300

    added svgSprite.svg & Icon.tsx

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,16 @@
+import svgSprite from '../../assets/icons/svgSprite.svg'
+
+type IconType = {
+    iconId: string
+    width?: string
+    height?: string
+    viewBox?: string
+}
+export const Icon = ({iconId, width, height, viewBox}: IconType) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={width || "50"} height={height || "50"}
+             viewBox={viewBox || "0 0 24 24"} fill="none">
+            <use xlinkHref={`${svgSprite}#${iconId}`}/>
+        </svg>
+    )
+}
