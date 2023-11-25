211285b4495f0681ef10d3803c79054e25bc90f0
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-25 18:41:14 +0300

    added props for Post;added style; added logic for Like

--- src/layout/profile/posts/post/Post.tsx
+++ src/layout/profile/posts/post/Post.tsx
@@ -1,12 +1,46 @@
 import {FC} from "react";
+import styled from "styled-components";
+import {Icon} from "../../../../components/icon/Icon";
+
 
 type PostType = {
+    id: number
     post: string
+    icon: string
+    like?: boolean
+    addLike: (id: number, newValue: boolean) => void
 }
-export const Post: FC<PostType> = ({post}: PostType) => {
+export const Post: FC<PostType> = ({id, post, icon, like, addLike}: PostType) => {
+    const colorLike = like ? "red" : "white"
+    const addLikeHandler = () => addLike(id, !like)
     return (
-        <div>
-            {post}
-        </div>
+        <StylePost>
+            <StyleImg src={icon} alt=""/>
+            <span>{post}</span>
+            <Icon iconId={"like"} height={"14"} width={"14"} viewBox={"0 0 48 48"} colorLike={colorLike} addLike={addLikeHandler} postId={id}/>
+        </StylePost>
     )
 }
+
+const StylePost = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: flex-end;
+  transition: .2s;
+
+  &:hover {
+
+  }
+
+  svg {
+    &:active {
+      transform: scale(0.9);
+    }
+  }
+`
+
+const StyleImg = styled.img`
+  height: 35px;
+  border-radius: 50%;
+  user-select: none;
+`
