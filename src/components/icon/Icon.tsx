eb18769335acaa9282b816b92b515a7eab51bee2
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-25 18:41:43 +0300

    added props for Icon;added style;

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -5,12 +5,15 @@ type IconType = {
     width?: string
     height?: string
     viewBox?: string
+    colorLike? : string
+    addLike?: () => void
+    postId: number
 }
-export const Icon = ({iconId, width, height, viewBox}: IconType) => {
+export const Icon = ({iconId, width, height, viewBox, colorLike, addLike}: IconType) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width={width || "50"} height={height || "50"}
-             viewBox={viewBox || "0 0 24 24"} fill="none">
-            <use xlinkHref={`${svgSprite}#${iconId}`}/>
+             viewBox={viewBox || "0 0 24 24"} fill="none" onClick={addLike}>
+            <use xlinkHref={`${svgSprite}#${iconId}`} color={colorLike}/>
         </svg>
     )
 }
