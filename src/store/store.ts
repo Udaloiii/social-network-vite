23269dd4d6dae434e474b8534995bf99286e1250
Udaloiii <udaloiiisohappy@gmail.com>
2023-11-27 21:24:25 +0300

    added usersReducer in store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import {combineReducers, legacy_createStore as createStore} from "redux";
 import {postsReducer} from "@/store/reducers/posts-reducer";
+import {usersReducer} from "@/store/reducers/users-reducer";
 
 const mainReducer = combineReducers({
-    posts: postsReducer
+    posts: postsReducer,
+    users: usersReducer
 })
 export const store = createStore(mainReducer)
 
