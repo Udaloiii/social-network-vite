0c0fb356a2952a11e4e5fcb2ad156accce610a98
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-01 22:26:47 +0300

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import {Header} from "@/layout/header/Header";
 import {FlexWrapper} from "@/components/flexWrapper/FlexWrapper";
 import {Navigation} from "@/layout/navigation/Navigation";
 import {Profile} from "@/layout/profile/Profile";
-import {Messages} from "@/layout/messages/Messages";
 import {News} from "@/layout/news/News";
 import {PageError} from "@/layout/pageError/PageError";
 import {Footer} from "@/layout/footer/Footer";
 import {Users} from "@/layout/users/Users";
+import {MessagesWithBlock} from "@/layout/messages/message/proba/MessagesWithBlock";
+import {Message} from "@/layout/messages/message/proba/Message";
 
 
 function App() {
@@ -18,10 +19,12 @@ function App() {
                 <Navigation/>
                 <Routes>
                     <Route path={'/'} element={<Profile/>}/>
-                    <Route path={'profile'} element={<Profile/>}/>
-                    <Route path={'users'} element={<Users/>}/>
-                    <Route path={'messages'} element={<Messages/>}/>
-                    <Route path={'news'} element={<News/>}/>
+                    <Route path={'/profile'} element={<Profile/>}/>
+                    <Route path={'/users'} element={<Users/>}/>
+                    {/*<Route path={'messages'} element={<Messages/>}/>*/}
+                    <Route path={'/messages'} element={<MessagesWithBlock/>}/>
+                    <Route path={'/messages/:id'} element={<Message/>}/>
+                    <Route path={'/news'} element={<News/>}/>
                     <Route path={'/*'} element={<PageError/>}/>
                 </Routes>
             </FlexWrapper>
