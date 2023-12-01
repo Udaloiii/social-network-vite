bab7eaf567e24b527025735a24841a7742deef6c
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-01 22:27:37 +0300

    added messagesReducer in store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import {combineReducers, legacy_createStore as createStore} from "redux";
 import {postsReducer} from "@/store/reducers/posts-reducer";
 import {usersReducer} from "@/store/reducers/users-reducer";
+import {messagesReducer} from "@/store/reducers/messages-reducer";
 
 const mainReducer = combineReducers({
     posts: postsReducer,
-    users: usersReducer
+    users: usersReducer,
+    messages: messagesReducer
 })
 export const store = createStore(mainReducer)
 
