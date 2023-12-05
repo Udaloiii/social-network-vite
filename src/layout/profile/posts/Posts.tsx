929622bb18d09aa38ba8d4141d08dfd40d3d0cc5
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-05 20:42:45 +0300

    added time in Posts.tsx for addNewPostHandler

--- src/layout/profile/posts/Posts.tsx
+++ src/layout/profile/posts/Posts.tsx
@@ -1,30 +1,41 @@
 import styled from "styled-components";
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 import {FlexWrapper} from "@/components/flexWrapper/FlexWrapper";
 import {AddItemForm} from "@/components/addItemForm/AddItemForm";
 import {Post} from "@/layout/profile/posts/post/Post";
 import {addLikeAC, addPostAC, PostType} from "@/store/reducers/posts-reducer";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "@/store/store";
+import {getTime} from "@/utils/getTime";
 
 
 export const Posts: FC = () => {
     const posts = useSelector<AppStateType, PostType[]>(state => state.posts)
     const dispatch = useDispatch()
 
+    const [time, setTime] = useState(new Date()) // для времени сообщения
+
     const addNewPostHandler = (text: string) => {
-        dispatch(addPostAC(text))
+        dispatch(addPostAC(text, getTime(time)))
     }
     const addLike = (id: number, newValue: boolean) => {
         dispatch(addLikeAC(id, newValue))
     }
+
+    useEffect(() => {
+        setTime(new Date())
+        console.log("useEffect в Message")
+    }, [posts]);
     return (
         <StylePosts>
             <FlexWrapper direction={"column"} gap={"20px"}>
                 <h4>Add post</h4>
-                <AddItemForm addItem={addNewPostHandler} as={"textarea"} placeholder = {"create you post"} buttonTitle={"add post"}/>
+                <AddItemForm addItem={addNewPostHandler} as={"textarea"} placeholder={"create you post"}
+                             buttonTitle={"add post"}/>
                 {posts.map((el, index) => {
                     return <Post key={index} id={el.id} icon={el.icon} post={el.text} like={el.like}
+                                 postTime={el.postTime}
+                                 postDate = {el.postDate}
                                  addLike={addLike}/>
                 })}
             </FlexWrapper>
