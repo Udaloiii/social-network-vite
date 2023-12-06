01d2251372d4d9b93b84ab99605cc97f13b2760e
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-06 20:12:28 +0300

    added messageId for addMessageAC; refactored

--- src/store/reducers/messages-reducer.ts
+++ src/store/reducers/messages-reducer.ts
@@ -1,27 +1,27 @@
 import {MessagesType} from "@/api/messages-api";
 import {getTime} from "@/utils/getTime";
 
-const initialState: MessagesType[] = []
+export type MessageStateType = MessagesType & { messageTime?: string, messageId: number }
+const initialState: MessageStateType[] = []
 // Type ACTION
 type ActionType = ReturnType<typeof setMessagesAC> | ReturnType<typeof addMessageAC>
 
 
 const time = getTime(new Date)
-export const messagesReducer = (state = initialState, action: ActionType): MessagesType[] => {
+export const messagesReducer = (state = initialState, action: ActionType): MessageStateType[] => {
     switch (action.type) {
         case "SET-MESSAGES":
-            return action.messages.map(el => ({...el, messageTime: time}))
+            return action.messages.map(el => ({...el, messageTime: time, messageId: el.id * 1000}))
 
         case "ADD-MESSAGE": {
-            const newMessage: MessagesType = {
-                id: action.idMessage,
+            const newMessage: MessageStateType = {
+                id: action.id,
                 name: "Username",
                 body: action.text,
-                messageTime: action.messageTime
+                messageTime: action.messageTime,
+                messageId: action.messageId
             }
-            const copyState = [...state, newMessage]
-            console.log(copyState)
-            return copyState
+            return [...state, newMessage]
         }
 
         default:
@@ -35,6 +35,6 @@ export const setMessagesAC = (messages: MessagesType[]) => {
     return {type: "SET-MESSAGES", messages} as const
 }
 
-export const addMessageAC = (idMessage: number, text: string, messageTime: string) => {
-    return {type: "ADD-MESSAGE", idMessage, text, messageTime} as const
+export const addMessageAC = (id: number, messageId: number, text: string, messageTime: string) => {
+    return {type: "ADD-MESSAGE", id, text, messageId, messageTime} as const
 }
