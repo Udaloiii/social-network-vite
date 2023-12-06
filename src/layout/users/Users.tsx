76d17f7ad9ef21485162ff5dc2869f60056c6383
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-06 20:20:35 +0300

    change logic; refactored

--- src/layout/users/Users.tsx
+++ src/layout/users/Users.tsx
@@ -5,19 +5,23 @@ import {usersApi, UserType} from "@/api/users-api";
 import {User} from "@/layout/users/user/User";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "@/store/store";
-import {setUsersAC, setUsersCountAC} from "@/store/reducers/users-reducer";
+import {changePageSizeAC, setUsersAC, setUsersCountAC} from "@/store/reducers/users-reducer";
 import {CustomSelect} from "@/components/customSelect/CustomSelect";
 import {Pagination} from "@/components/pagination/Pagination";
 import {Loader} from "@/components/loader/Loader";
 
 
 export const Users: FC = () => {
-    const users = useSelector<AppStateType, UserType[]>(state => state.users.items)
+    const users = useSelector<AppStateType, Array<UserType & {
+        posts: string[],
+        icon: string
+    }>>(state => state.users.items)
+    const pageSize = useSelector<AppStateType, number>(state => state.users.pageSize || 10)
+
     const usersCount = useSelector<AppStateType, number>(state => state.users.totalCount)
     const dispatch = useDispatch()
 
     const [currentPage, setCurrentPage] = useState<number>(1)
-    const [pageSize, setPageSize] = useState(10)
     const [totalCount, setTotalCount] = useState(usersCount)
 
 
@@ -28,11 +32,12 @@ export const Users: FC = () => {
     // }, [users, currentPage, pageSize])
 
     const setPageSizeHandler = (pageSize: number) => {
-        setPageSize(pageSize)
+        dispatch(changePageSizeAC(pageSize))
         setCurrentPage(1)
     }
 
     useEffect(() => {
+        // console.log(users?.length)
         usersApi.getUser(pageSize, currentPage)
             .then(res => {
                 console.log(res.data)
@@ -40,7 +45,7 @@ export const Users: FC = () => {
                 dispatch(setUsersAC((res.data.items)))
                 dispatch(setUsersCountAC((res.data.totalCount)))
             })
-    }, [pageSize, currentPage, dispatch]);
+    }, [users?.length, pageSize, usersCount, currentPage, dispatch]);
 
 
     return (
@@ -49,7 +54,8 @@ export const Users: FC = () => {
                 <CustomSelect title={"юзеров"} value={pageSize} options={[10, 25, 50]}
                               changePageSize={setPageSizeHandler}/>
                 <FlexWrapper direction={"column"} gap={"30px"}>
-                    {users?.map(el => <User key={el.id} id={el.id} name={el.name} followed={el.followed}/>
+                    {users?.map(el => <User key={el.id} userId={el.id} name={el.name} followed={el.followed}
+                                            icon={el.icon}/>
                     )}
                 </FlexWrapper>
                 <PaginationWrapper>
