22b4854f1e29bbecc7273e88c8785e3b72f27377
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-06 20:21:08 +0300

    changed UserInfo.tsx; changed style

--- src/layout/profile/userInfo/UserInfo.tsx
+++ src/layout/profile/userInfo/UserInfo.tsx
@@ -1,14 +1,23 @@
-import { FlexWrapper } from "@/components/flexWrapper/FlexWrapper"
+import {FlexWrapper} from "@/components/flexWrapper/FlexWrapper"
+import {UserType} from "@/api/users-api";
+import styled from "styled-components";
 
-
-export const UserInfo = () => {
+type UserInfoPropsType = {
+    user?: UserType
+}
+export const UserInfo = ({user}: UserInfoPropsType) => {
     return (
         <FlexWrapper direction={"column"} justify={"space-between"} style={{margin: "0"}}>
-            <div>Name</div>
-            <div>Date of Birth</div>
+            <span>Name: <StyleName>{user?.name}</StyleName></span>
+            <div>Date of Birth:</div>
             <div>City</div>
             <div>Country</div>
             <div>Education</div>
         </FlexWrapper>
     )
 }
+
+const StyleName = styled.span`
+  text-transform: capitalize;
+  font-family: Algerian, sans-serif;
+`
