d79cfede4354f6398762083eed912b8148bdbdb0
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-09 00:29:43 +0300

    added profileReducer in store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,18 @@
 import {combineReducers, legacy_createStore as createStore} from "redux";
-import {postsReducer} from "@/store/reducers/posts-reducer";
+// import {postsReducer} from "@/store/reducers/posts-reducer";
 import {usersReducer} from "@/store/reducers/users-reducer";
 import {messagesReducer} from "@/store/reducers/messages-reducer";
+import {profileReducer} from "@/store/reducers/profile-reducer";
 
 const mainReducer = combineReducers({
-    posts: postsReducer,
+    // posts: postsReducer,
     users: usersReducer,
-    messages: messagesReducer
+    messages: messagesReducer,
+    profile: profileReducer // потом можно убрать
 })
 export const store = createStore(mainReducer)
 
 export type AppStateType = ReturnType<typeof mainReducer>
+
+// @ts-ignore
+window.store = store
