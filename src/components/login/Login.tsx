97fc4a3f15539704859a6b30dea51a7aab38bb6c
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-13 14:09:59 +0300

    created Login.tsx

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,21 @@
+export const Login = () => {
+    return (
+        <div className="login-box">
+            <form>
+                <div className="user-box">
+                    <input type="text" name="" required/>
+                    <label>Username</label>
+                </div>
+                <div className="user-box">
+                    <input type="password" name="" required/>
+                    <label>Password</label>
+                </div>
+                <center>
+                    <a href="#">
+                        SEND
+                        <span></span>
+                    </a></center>
+            </form>
+        </div>
+    )
+}
