a8b67002af18d5a5a49db7ea361aa2c8cdb30697
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-16 22:03:54 +0300

    added redux-thunk in App for thunk (version "^2.4.2", latest show error)

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,20 +1,28 @@
-import {combineReducers, legacy_createStore as createStore} from "redux";
-// import {postsReducer} from "@/store/reducers/posts-reducer";
+import {Action, applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
 import {usersReducer} from "@/store/reducers/users-reducer";
 import {messagesReducer} from "@/store/reducers/messages-reducer";
 import {profileReducer} from "@/store/reducers/profile-reducer";
 import {newsReducer} from "@/store/reducers/news-reducer";
+import {appReducer} from "@/store/reducers/app-reducer";
+import {authReducer} from "@/store/reducers/auth-reducer";
+import thunk, {ThunkDispatch} from "redux-thunk";
+import {useDispatch} from "react-redux";
 
 const mainReducer = combineReducers({
     // posts: postsReducer,
+    app: appReducer,
+    auth: authReducer,
     users: usersReducer,
     messages: messagesReducer,
     profile: profileReducer, // потом можно убрать
-    news: newsReducer
+    news: newsReducer,
 })
-export const store = createStore(mainReducer)
+export const store = createStore(mainReducer, applyMiddleware(thunk))
 
 export type AppStateType = ReturnType<typeof mainReducer>
+
+type AppDispatchType = ThunkDispatch<AppStateType, never, Action> // вместо any поставил never (ругалось)
+export const useAppDispatch = () => useDispatch<AppDispatchType>()
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
