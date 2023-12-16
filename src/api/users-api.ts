6941771ed2f94e10648860d99f40d4e8697cdc66
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-16 22:06:01 +0300

    refactored

--- src/api/users-api.ts
+++ src/api/users-api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import {instance} from "@/api/general-axios-instace";
 
 
 export type UserType = {
@@ -37,13 +37,6 @@ export type UsersResponseType = {
     totalCount: number
     error: null | string
 }
-const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    withCredentials: true,
-    headers: {
-        'API-KEY': 'bfb2a9e7-51b4-4737-9da8-c82e19cdd77e'
-    }
-})
 
 export const usersApi = {
     getUsers(usersCount: number, currentPage: number) {
