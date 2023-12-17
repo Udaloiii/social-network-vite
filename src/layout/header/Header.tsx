e3a7935ff701485f83c0c6413cb77c88a21b87ee
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-17 09:30:10 +0300

    added Button.tsx (logout) in header; added logoutHandler for this

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,15 +1,29 @@
 import styled from "styled-components";
 import {FC} from "react";
+import {Button} from "@/components/button/Button";
+import {logOutTC} from "@/store/reducers/auth-reducer";
+import {useAppDispatch} from "@/store/store";
 
 export const Header: FC = () => {
+    const dispatch = useAppDispatch()
+    const logoutHandler = () => dispatch(logOutTC())
     return (
         <HeaderStyled>
             Header
+            <Button text={"logout"} addItem={logoutHandler}/>
         </HeaderStyled>
     )
 }
 
 const HeaderStyled = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   height: 60px;
   background-color: #c4ffff;
+
+  button {
+    margin-right: 20px;
+    background-color: transparent;
+  }
 `
