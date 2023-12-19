33576c7bf57462ad47d1f57c5c49a6580b276074
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-19 23:30:25 +0300

    added logic; changed styles

--- src/layout/profile/userInfo/UserInfo.tsx
+++ src/layout/profile/userInfo/UserInfo.tsx
@@ -1,22 +1,59 @@
 import {FlexWrapper} from "@/components/flexWrapper/FlexWrapper"
-import {ProfileResponseType} from "@/api/users-api";
 import styled from "styled-components";
+import {UserItemType} from "@/store/reducers/users-reducer";
+import {ProfileResponseType} from "@/api/users-api";
 
 type UserInfoPropsType = {
-    user?: ProfileResponseType
+    profile?: ProfileResponseType
+    user?: UserItemType
 }
-export const UserInfo = ({user}: UserInfoPropsType) => {
+export const UserInfo = ({profile, user}: UserInfoPropsType) => {
     return (
-        <FlexWrapper direction={"column"} justify={"space-between"} style={{margin: "0"}}>
-            <div>Name: <StyleName>{user?.fullName}</StyleName></div>
-            <div>Date of Birth: 01.01.1990</div>
-            <div>City: Sidney</div>
-            <div>Country: Australia</div>
-        </FlexWrapper>
+        <UserInfoWrapper>
+            <FlexWrapper direction={"column"} justify={"space-between"} style={{margin: "0"}}>
+                <InfoWrap>
+                    <StyleDescription>Name:</StyleDescription>
+                    <StyleName>{user?.name || profile?.fullName}</StyleName>
+                </InfoWrap>
+                <InfoWrap>
+                    <StyleDescription>Date of Birth:</StyleDescription>
+                    {user?.dateOfBorn || 1990}
+                </InfoWrap>
+                <InfoWrap><StyleDescription>City:</StyleDescription>{user?.city || "Minsk"}</InfoWrap>
+                <InfoWrap><StyleDescription>Country: </StyleDescription>{user?.country || "Belarus"}</InfoWrap>
+            </FlexWrapper>
+        </UserInfoWrapper>
     )
 }
 
+const UserInfoWrapper = styled.div`
+  padding: 15px;
+  min-width: 250px;
+  width: 450px;
+  display: flex;
+  border-radius: 10px;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+
+  ${FlexWrapper} {
+    width: 100%;
+  }
+`
+
 const StyleName = styled.span`
   text-transform: capitalize;
-  font-family: Algerian, sans-serif;
+  font-family: "Special Elite", sans-serif;
+  color: royalblue;
+  letter-spacing: 2px;
+  font-size: 1.5rem;
+`
+
+const InfoWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`
+
+const StyleDescription = styled.div`
+  font-family: "Josefin Sans", sans-serif;
 `
