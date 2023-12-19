6efcf6db9731e1d23e3d1ea0ab7673d1a7378750
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-19 21:56:26 +0300

    changed style; refactored

--- src/layout/navigation/Navigation.tsx
+++ src/layout/navigation/Navigation.tsx
@@ -26,8 +26,8 @@ const StyleNavigation = styled.nav`
   padding-top: 20px;
   background-color: rgba(23, 23, 23, 0.8);
   min-height: calc(100vh - 110px);
-  font-family: "Broadway", sans-serif;
-  font-size: 1.2rem;
+  font-family: "Rubik Doodle Shadow", sans-serif;
+  font-size: 1.1rem;
 `
 const StyleLink = styled(NavLink)`
   position: relative;
@@ -59,23 +59,13 @@ const StyleLink = styled(NavLink)`
     color: royalblue;
     transition: .1s;
     transform: scale(1.3);
-    //text-decoration: underline;
     letter-spacing: 0;
-    //text-shadow: 0 0 5px royalblue;
     text-shadow: 1px 1px 1px black;
 
     &::before {
-      //content: "";
-      //position: absolute;
-      //top: 50%;
-      //transform: translateY(-50%);
-      //right: -12px;
-      //width: 0;
-      //height: 0;
       border-top: 10px solid transparent;
       border-bottom: 10px solid transparent;
       border-right: 10px solid #c9ffeb;
-      //transform: rotate(180deg);
       transition: .2s ease-in-out;
     }
   }
