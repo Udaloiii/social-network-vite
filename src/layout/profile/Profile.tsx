b65f7c81b3050404cc5c5dfbead8ee14f03e6ccb
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-19 23:32:27 +0300

    added logic; added getProfileTC for useEffect

--- src/layout/profile/Profile.tsx
+++ src/layout/profile/Profile.tsx
@@ -3,13 +3,13 @@ import {FC, useEffect} from "react";
 import {FlexWrapper} from "@/components/flexWrapper/FlexWrapper";
 import {UserInfo} from "@/layout/profile/userInfo/UserInfo";
 import {Posts} from "@/layout/profile/posts/Posts";
-import {usersApi} from "@/api/users-api";
 import {useSelector} from "react-redux";
 import {AppStateType, useAppDispatch} from "@/store/store";
-import {ProfileType, setProfileAC} from "@/store/reducers/profile-reducer";
+import {getProfileTC, ProfileType} from "@/store/reducers/profile-reducer";
 import {Navigate, useParams} from "react-router-dom";
 import {UserItemType} from "@/store/reducers/users-reducer";
 import {Loader1} from "@/components/loader/Loader1";
+import userAvatar from '../../assets/avatars/user.webp'
 
 
 export const Profile: FC = () => {
@@ -18,18 +18,16 @@ export const Profile: FC = () => {
     const user = useSelector<AppStateType, UserItemType[]>(state => state.users.items)
     const dispatch = useAppDispatch()
     const params = useParams()
-    const id = params.id ? +params.id : 26652
-    const image = user?.filter(el => el.id === id)[0]?.icon || "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=1480&t=st=1700817612~exp=1700818212~hmac=86a79fc7b83745f8e03378e58710b0b6c590f19d1d6a624ff5bc2227c790e259"
-    // const posts = user?.filter(el => el.id === id)[0]?.posts
-    const posts = profile.posts
-    console.log(posts)
+    const userId = useSelector<AppStateType, number>(state => state.auth.myId)
+    const id = params.id ? +params.id : userId
+    const image = user?.filter(el => el.id === id)[0]?.icon || userAvatar
+    const posts = user?.filter(el => el.id === id)[0]?.posts || profile.posts
+
+
 
     useEffect(() => {
-        usersApi.getMyProfile(id)
-            .then(res => {
-                dispatch(setProfileAC(id, res.data, image))
-            })
-    }, [profile.userId, id, dispatch, image]);
+        dispatch(getProfileTC(id, image))
+    }, [profile.userId, dispatch, id, image, userId, profile.fullName]);
 
     if (!isInitialized) {
         return <Navigate to={'/login'}/>
@@ -43,7 +41,10 @@ export const Profile: FC = () => {
                         <StyleProfileImage
                             src={image}
                         />
-                        <UserInfo user={profile}/>
+                        <UserInfo
+                            user={user?.filter(el => el.id === id)[0]}
+                            profile={profile}
+                        />
                     </FlexWrapper>
                     <Posts userId={id} posts={posts}/>
                 </ProfileWrapper>
@@ -62,7 +63,7 @@ const ProfileWrapper = styled.div`
   flex-direction: column;
   gap: 100px;
   padding: 20px;
-  
+
   textarea {
     width: 325px;
   }
@@ -72,4 +73,5 @@ const StyleProfileImage = styled.img`
   height: 290px;
   object-fit: cover;
   border-radius: 10px;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 `
