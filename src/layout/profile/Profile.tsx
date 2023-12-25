b5c9e58792d411cf51b87e8790519f13222ecb6c
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-25 17:02:54 +0300

    refactored; changed style

--- src/layout/profile/Profile.tsx
+++ src/layout/profile/Profile.tsx
@@ -10,10 +10,12 @@ import {Navigate, useParams} from "react-router-dom";
 import {UserItemType} from "@/store/reducers/users-reducer";
 import {Loader} from "@/components/loader/Loader";
 import userAvatar from '../../assets/avatars/user.webp'
-import background from '../../assets/backgrounds/3384318_57622.jpg'
+import background from '../../assets/backgrounds/white-2484120_1920.png'
+import {RequestStatusType} from "@/store/reducers/app-reducer";
 
 
 export const Profile: FC = () => {
+    const isLoading = useSelector<AppStateType, RequestStatusType>(state => state.app.status)
     const isInitialized = useSelector<AppStateType, boolean>(state => state.auth.isLoggedIn)
     const profile = useSelector<AppStateType, ProfileType>(state => state.profile)
     const user = useSelector<AppStateType, UserItemType[]>(state => state.users.items)
@@ -25,10 +27,10 @@ export const Profile: FC = () => {
     const posts = user?.filter(el => el.id === id)[0]?.posts || profile.posts
 
 
-
     useEffect(() => {
         dispatch(getProfileTC(id, image))
-    }, [profile.userId, dispatch, id, image, userId, profile.fullName]);
+    // }, [profile.userId, dispatch, id, image, userId, profile.fullName]);
+    }, [dispatch, id]);
 
     if (!isInitialized) {
         return <Navigate to={'/login'}/>
@@ -36,8 +38,9 @@ export const Profile: FC = () => {
 
     return (
         <StyleProfile>
-            {profile.fullName ?
-                <ProfileWrapper>
+            {/*{profile.fullName ?*/}
+            {isLoading === "loading" ? <Loader/>
+                : <ProfileWrapper>
                     <FlexWrapper gap={"50px"}>
                         <StyleProfileImage
                             src={image}
@@ -49,31 +52,20 @@ export const Profile: FC = () => {
                     </FlexWrapper>
                     <Posts userId={id} posts={posts}/>
                 </ProfileWrapper>
-                : <Loader/>}
-        </StyleProfile>
+            }
 
-    // <StyleProfile>
-    //         <ProfileWrapper>
-    //             <FlexWrapper gap={"50px"}>
-    //                 <StyleProfileImage
-    //                     src={image}
-    //                 />
-    //                 <UserInfo
-    //                     user={user?.filter(el => el.id === id)[0]}
-    //                     profile={profile}
-    //                 />
-    //             </FlexWrapper>
-    //             <Posts userId={id} posts={posts}/>
-    //         </ProfileWrapper>
-    // </StyleProfile>
+        </StyleProfile>
     )
 }
 
 const StyleProfile = styled.section`
-  //background: url(${background}) 0 0/250px 250px repeat;
-  background: linear-gradient(180deg, #1485e6,#00c2f0,#2ceeb8);
-  //flex-grow: 1;
-  width: calc(100vw - 150px);
+    //background: url(${background}) 0 0/350px repeat;
+  //background: linear-gradient(180deg, #1485e6, #00c2f0, #2ceeb8);
+  flex-grow: 1;
+  //width: calc(100vw - 150px);
+  background: url("https://img.freepik.com/free-vector/dark-hexagonal-background-with-gradient-color_79603-1410.jpg?w=2000&t=st=1703507059~exp=1703507659~hmac=d30ca8681cb5c569e815bebb5cbc2528b0d3f96c80c438e5075c9b73b31f6e98") 0 0/ 350px repeat;
+  border: 1px solid rgba(128, 128, 128, 0.8);
+  border-right: none;
 `
 const ProfileWrapper = styled.div`
   display: flex;
@@ -86,8 +78,8 @@ const ProfileWrapper = styled.div`
   }
 `
 const StyleProfileImage = styled.img`
-  width: 325px;
-  height: 290px;
+  max-width: 325px;
+  max-height: 290px;
   object-fit: cover;
   border-radius: 10px;
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
