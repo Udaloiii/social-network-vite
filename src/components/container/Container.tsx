bacfe98ae9652a377773272e8928eeb8c6f57d61
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-28 23:53:32 +0300

    created Container.tsx; added in project

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  max-width: 1440px;
+  width: 100%;
+  //height: 100%;
+  margin: 0 auto;
+  //padding: 0 15px;
+  //background-color: #1f1f1f;
+`
