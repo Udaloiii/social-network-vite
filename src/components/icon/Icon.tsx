2f05a0eedf7922d68792c505604339a2113db890
Udaloiii <udaloiiisohappy@gmail.com>
2023-12-28 23:58:11 +0300

    added examination vkIcons?

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,18 +1,20 @@
 import svgSprite from '../../assets/icons/svgSprite.svg'
+import vkSprite from '../../assets/icons/svgSpriteVK.svg'
 
 type IconType = {
     iconId: string
     width?: string
     height?: string
     viewBox?: string
-    colorLike? : string
+    colorLike?: string
     addLike?: () => void
+    vkIcons?: boolean
 }
-export const Icon = ({iconId, width, height, viewBox, colorLike, addLike}: IconType) => {
+export const Icon = ({iconId, width, height, viewBox, colorLike, addLike, vkIcons}: IconType) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width={width || "50"} height={height || "50"}
              viewBox={viewBox || "0 0 24 24"} fill="none" onClick={addLike}>
-            <use xlinkHref={`${svgSprite}#${iconId}`} color={colorLike}/>
+            <use xlinkHref={`${vkIcons ? vkSprite : svgSprite}#${iconId}`} color={colorLike}/>
         </svg>
     )
 }
