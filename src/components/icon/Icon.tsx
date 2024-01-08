2246506524246bf5b6076301253d09e43f67297f
Udaloiii <udaloiiisohappy@gmail.com>
2024-01-08 22:56:27 +0300

    added onClick in Icon.tsx

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -9,12 +9,15 @@ type IconType = {
     colorLike?: string
     addLike?: () => void
     vkIcons?: boolean
+    onClick?: () => void
 }
-export const Icon = ({iconId, width, height, viewBox, colorLike, addLike, vkIcons}: IconType) => {
+export const Icon = ({iconId, width, height, viewBox, colorLike, addLike, vkIcons, onClick}: IconType) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={width || "50"} height={height || "50"}
-             viewBox={viewBox || "0 0 24 24"} fill="none" onClick={addLike}>
-            <use xlinkHref={`${vkIcons ? vkSprite : svgSprite}#${iconId}`} color={colorLike}/>
-        </svg>
+        <div onClick={onClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" width={width || "50"} height={height || "50"}
+                 viewBox={viewBox || "0 0 24 24"} fill="none" onClick={addLike}>
+                <use xlinkHref={`${vkIcons ? vkSprite : svgSprite}#${iconId}`} color={colorLike}/>
+            </svg>
+        </div>
     )
 }
