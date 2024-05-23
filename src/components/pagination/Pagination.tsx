0af3a71bd48c3ae530cbc03e85866fe159ae512d
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-23 13:16:33 +0300

    style: changed style in pagination

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -69,30 +69,30 @@ export const Pagination = ({
 }
 
 const PaginationItem = styled.li<{ condition?: boolean, disabled?: boolean }>`
-  padding: 0 12px;
-  height: 32px;
-  text-align: center;
-  margin: auto 4px;
-  display: flex;
-  box-sizing: border-box;
-  align-items: center;
-  letter-spacing: 0.01071em;
-  border-radius: 16px;
-  line-height: 1.43;
-  font-size: 13px;
-  min-width: 32px;
-  cursor: pointer;
-  background-color: ${props => props.condition && "royalblue"};
-  color: ${props => props.condition && "whitesmoke"};
-  transition: .2s;
-  user-select: none;
-  
-  &:hover {
-    background-color: ${props => !props.condition && "rgba(0, 0, 0, 0.08)"};
-  }
+    padding: 0 12px;
+    height: 32px;
+    margin: auto 4px;
+    display: flex;
+    box-sizing: border-box;
+    align-items: center;
+    justify-content: center;
+    letter-spacing: 0.01071em;
+    border-radius: 16px;
+    line-height: 1.43;
+    font-size: 13px;
+    min-width: 32px;
+    cursor: pointer;
+    background-color: ${props => props.condition && "royalblue"};
+    color: ${props => props.condition && "whitesmoke"};
+    transition: .2s;
+    user-select: none;
 
-  pointer-events: ${props => props.disabled && "none"};
-  opacity: ${props => props.disabled && "0.3"};
+    &:hover {
+        background-color: ${props => !props.condition && "rgba(0, 0, 0, 0.08)"};
+    }
+
+    pointer-events: ${props => props.disabled && "none"};
+    opacity: ${props => props.disabled && "0.3"};
 `
 
 const PaginationContainer = styled.ul`
@@ -115,8 +115,6 @@ const Arrow = styled.div`
     display: inline-block;
     width: 0.4em;
     height: 0.4em;
-    //border-right: 0.12em solid rgba(0, 0, 0, 0.87);
-    //border-top: 0.12em solid rgba(0, 0, 0, 0.87);
     border-right: 0.12em solid currentColor;
     border-top: 0.12em solid currentColor;
   }
